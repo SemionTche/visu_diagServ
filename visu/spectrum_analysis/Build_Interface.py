12ca8d5c4e77161a277850bd28bae937c8cf8c0c
Aline Vernier <aline.vernier@ensta.fr>
2025-12-23 16:01:20 +0100

    Added all input interface elements
    To be connected to backend
    Config read/write still to be implemented

--- visu/spectrum_analysis/Build_Interface.py
+++ visu/spectrum_analysis/Build_Interface.py
@@ -3,14 +3,11 @@
 """
 Created on 2025/12/16
 @author: Aline Vernier
-Build Spectrometer Interface
+Build Spectrometer Interface - GUI only
 """
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget, QHBoxLayout, QGridLayout
-from PyQt6.QtWidgets import (QLabel, QMainWindow, QFileDialog, QStatusBar,
+from PyQt6.QtWidgets import (QLabel, QMainWindow, QStatusBar, QComboBox,
                              QCheckBox, QDoubleSpinBox,  QPushButton, QLineEdit)
-from PyQt6 import QtCore, QtGui
-from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QAction
 from PyQt6.QtGui import QIcon, QFont
 import sys
 import pyqtgraph as pg
@@ -28,6 +25,7 @@ class Spectrometer_Interface(QMainWindow):
         self.icon = str(p.parent.parent) + sepa + 'icons' + sepa
         print(self.icon)
         self.setup()
+        self.action_button()
 
     def setup(self):
         #####################################################################
@@ -67,6 +65,7 @@ class Spectrometer_Interface(QMainWindow):
         self.hbox = QHBoxLayout()
         MainWidget = QWidget()
         MainWidget.setLayout(self.hbox)
+        blank_space = QLabel('')
         self.setCentralWidget(MainWidget)
 
         # LHS vertical box with stacked graphs
@@ -85,16 +84,17 @@ class Spectrometer_Interface(QMainWindow):
         self.vbox2.addLayout(title_layout)
         Title = QLabel('Controls and indicators')
         Title.setFont(QFont('Arial', 14))
-        ph_1 = QLabel()
-        ph_2 = QLabel()
-        title_layout.addWidget(ph_1, 0, 0)
+        title_layout.addWidget(blank_space, 0, 0)
         title_layout.addWidget(Title, 0, 1)
-        title_layout.addWidget(ph_2, 0, 2)
+        title_layout.addWidget(blank_space, 0, 2)
 
         #####################################################################
         #       Fill layout with graphs, controls and indicators
         #####################################################################
 
+        ######################
+        #       Graphs
+        ######################
         # 2D plot (image histogram) in LHS vbox
         self.winImage = pg.GraphicsLayoutWidget()
         self.vbox1.addWidget(self.winImage)
@@ -121,6 +121,9 @@ class Spectrometer_Interface(QMainWindow):
         self.dnde_image = self.graph_widget.addPlot()
         self.dnde_image.setContentsMargins(10, 10, 10, 10)
 
+        ######################
+        #    Lock Controls
+        ######################
         # Controls and indicators, labels
         grid_layout_enable_controls = QGridLayout()
         lock_controls = QLabel('Lock controls')
@@ -132,6 +135,18 @@ class Spectrometer_Interface(QMainWindow):
         grid_layout_enable_controls.addWidget(self.locked_unlocked, 0, 2)
         self.vbox2.addLayout(grid_layout_enable_controls)
 
+        ######################
+        # E, ds/dE, s config
+        ######################
+
+        grid_layout_config_title = QGridLayout()
+        calibration_label = QLabel('Calibration')
+        calibration_label.setFont(QFont('Arial', 11))
+        grid_layout_config_title.addWidget(blank_space, 0, 0)
+        grid_layout_config_title.addWidget(calibration_label, 0, 1)
+        grid_layout_config_title.addWidget(blank_space, 0, 2)
+        self.vbox2.addLayout(grid_layout_config_title)
+
         grid_layout_config = QGridLayout()
         self.config_path_button = QPushButton('Path : ')
         self.config_path_button.setFixedWidth(50)
@@ -141,35 +156,98 @@ class Spectrometer_Interface(QMainWindow):
         grid_layout_config.addWidget(self.config_path_box, 0, 1)
         self.vbox2.addLayout(grid_layout_config)
 
+        ######################
+        # Calibration layout
+        ######################
+
+        grid_layout_calib = QGridLayout()
+
+        px_per_mm_label = QLabel('px/mm')
+        mrad_per_px_label = QLabel('mrad/px')
+        pC_per_count_label = QLabel('pC/count ×10<sup>-6</sup>')
+        reference_label = QLabel('Ref. Method')
+
+        self.px_per_mm_ctl = QDoubleSpinBox()
+        self.mrad_per_px_ctl = QDoubleSpinBox()
+        self.pC_per_count_ctl = QDoubleSpinBox()
+        self.reference_method = QComboBox()
+        self.refpoint_x_or_energy = QDoubleSpinBox()
+        self.refpoint_y_or_s = QDoubleSpinBox()
+
+        self.reference_method.addItem('Zero')
+        self.reference_method.addItem('RefPoint')
+        self.reference_pts_label = QLabel('(x, y): (px, px)')
+
+        self.pC_per_count_ctl.setMinimum(0.001)
+        self.pC_per_count_ctl.setMaximum(1000)
+        self.pC_per_count_ctl.setValue(4.33)
+
+        self.mrad_per_px_ctl.setMinimum(1e-6)
+        self.mrad_per_px_ctl.setMaximum(1)
+        self.mrad_per_px_ctl.setValue(0.1)
+
+        self.px_per_mm_ctl.setMinimum(1.)
+        self.px_per_mm_ctl.setMaximum(10000.)
+        self.px_per_mm_ctl.setValue(20.408)
+
+        self.refpoint_x_or_energy.setMinimum(0)
+        self.refpoint_x_or_energy.setMaximum(10000)
+        self.refpoint_x_or_energy.setValue(2000)
+
+        self.refpoint_x_or_energy.setMinimum(0)
+        self.refpoint_x_or_energy.setMaximum(10000)
+        self.refpoint_x_or_energy.setValue(2000)
+
+        self.refpoint_y_or_s.setMinimum(0)
+        self.refpoint_y_or_s.setMaximum(10000)
+        self.refpoint_y_or_s.setValue(650)
+
+        grid_layout_calib.addWidget(px_per_mm_label, 0, 0)
+        grid_layout_calib.addWidget(self.px_per_mm_ctl, 0, 1)
+        grid_layout_calib.addWidget(mrad_per_px_label, 1, 0)
+        grid_layout_calib.addWidget(self.mrad_per_px_ctl, 1, 1)
+        grid_layout_calib.addWidget(reference_label, 2, 0)
+        grid_layout_calib.addWidget(self.reference_method, 2, 1)
+        grid_layout_calib.addWidget(self.reference_pts_label, 3, 0)
+        grid_layout_calib.addWidget(self.refpoint_x_or_energy, 3, 1)
+        grid_layout_calib.addWidget(self.refpoint_y_or_s, 3, 2)
+        grid_layout_calib.addWidget(pC_per_count_label, 4, 0)
+        grid_layout_calib.addWidget(self.pC_per_count_ctl, 4, 1)
+        self.vbox2.addLayout(grid_layout_calib)
+
         self.flip_image = QCheckBox('Deflect.: R to L?', self)
         self.flip_image.setChecked(True)
 
         lanex_offset_label = QLabel('Lanex Offset (+ to low E)')
-        self.lanex_offset_mm_control = QDoubleSpinBox()
-        self.lanex_offset_mm_control.setValue(0)
-        self.lanex_offset_mm_control.setSingleStep(.1)
-        self.lanex_offset_mm_control.setMinimum(-100.)
-        self.lanex_offset_mm = self.lanex_offset_mm_control.value()
+        self.lanex_offset_mm_ctl = QDoubleSpinBox()
+        self.lanex_offset_mm_ctl.setFixedWidth(80)
+        self.lanex_offset_mm_ctl.setValue(0)
+        self.lanex_offset_mm_ctl.setSingleStep(.1)
+        self.lanex_offset_mm_ctl.setMinimum(-100.)
+        self.lanex_offset_mm = self.lanex_offset_mm_ctl.value()
 
         cutoff_energies_label = QLabel('Cutoff energies (MeV)')
-        self.min_cutoff_energy_control = QDoubleSpinBox()  # for the value
-        self.min_cutoff_energy_control.setValue(10)
-        self.min_cutoff_energy = self.min_cutoff_energy_control.value()
-        self.min_cutoff_energy_control.setMinimum(0)
-        self.min_cutoff_energy_control.setSingleStep(1)
+        self.min_cutoff_energy_ctl = QDoubleSpinBox()
+        self.min_cutoff_energy_ctl.setFixedWidth(80)
+        self.min_cutoff_energy_ctl.setValue(10)
+        self.min_cutoff_energy = self.min_cutoff_energy_ctl.value()
+        self.min_cutoff_energy_ctl.setMinimum(0)
+        self.min_cutoff_energy_ctl.setSingleStep(1)
 
-        self.max_cutoff_energy_control = QDoubleSpinBox()  # for the value
-        self.max_cutoff_energy_control.setValue(200)
-        self.max_cutoff_energy = self.max_cutoff_energy_control.value()
-        self.max_cutoff_energy_control.setMinimum(50)
-        self.max_cutoff_energy_control.setSingleStep(10)
+        self.max_cutoff_energy_ctl = QDoubleSpinBox()
+        self.max_cutoff_energy_ctl.setFixedWidth(80)
+        self.max_cutoff_energy_ctl.setValue(200)
+        self.max_cutoff_energy = self.max_cutoff_energy_ctl.value()
+        self.max_cutoff_energy_ctl.setMinimum(50)
+        self.max_cutoff_energy_ctl.setSingleStep(10)
 
         energy_resolution_label = QLabel('Energy resolution (MeV)')
         self.energy_resolution_ctl = QDoubleSpinBox()
+        self.energy_resolution_ctl.setFixedWidth(80)
         self.energy_resolution_ctl.setValue(0.5)
-        self.max_cutoff_energy_control.setMinimum(0.1)
-        self.max_cutoff_energy_control.setMaximum(10)
-        self.max_cutoff_energy_control.setSingleStep(0.5)
+        self.max_cutoff_energy_ctl.setMinimum(0.1)
+        self.max_cutoff_energy_ctl.setMaximum(10)
+        self.max_cutoff_energy_ctl.setSingleStep(0.5)
 
         # Fill grid with controls and indicators
         self.grid_layout = QGridLayout()
@@ -178,20 +256,34 @@ class Spectrometer_Interface(QMainWindow):
         self.grid_layout.addWidget(QLabel(), 1, 2)
         self.grid_layout.addWidget(self.flip_image, 2, 0)
         self.grid_layout.addWidget(cutoff_energies_label, 3, 0)
-        self.grid_layout.addWidget(self.min_cutoff_energy_control, 3, 2)
-        self.grid_layout.addWidget(self.max_cutoff_energy_control, 3, 3)
+        self.grid_layout.addWidget(self.min_cutoff_energy_ctl, 3, 2)
+        self.grid_layout.addWidget(self.max_cutoff_energy_ctl, 3, 3)
         self.grid_layout.addWidget(lanex_offset_label, 4, 0)
-        self.grid_layout.addWidget(self.lanex_offset_mm_control, 4, 3)
+        self.grid_layout.addWidget(self.lanex_offset_mm_ctl, 4, 3)
+        self.grid_layout.addWidget(energy_resolution_label, 5, 0)
+        self.grid_layout.addWidget(self.energy_resolution_ctl, 5, 3)
 
         #####################################################################
         #                       Interface actions
         #####################################################################
 
     def action_button(self) -> None:
-        self.min_cutoff_energy_control.valueChanged.connect(self.change_energy_bounds)
-        self.max_cutoff_energy_control.valueChanged.connect(self.change_energy_bounds)
-        self.lanex_offset_mm_control.valueChanged.connect(self.change_lanex_offset_mm)
+        self.min_cutoff_energy_ctl.valueChanged.connect(self.change_energy_bounds)
+        self.max_cutoff_energy_ctl.valueChanged.connect(self.change_energy_bounds)
+        self.lanex_offset_mm_ctl.valueChanged.connect(self.change_lanex_offset_mm)
         self.enable_controls.stateChanged.connect(self.enable_disable_controls)
+        self.flip_image.stateChanged.connect(self.clear_graph)
+        self.reference_method.currentTextChanged.connect(self.update_refpoint)
+
+    def update_refpoint(self):
+        if self.reference_method.currentText() == "Zero":
+            self.reference_pts_label.setText('(x, y): (px, px)')
+        else:
+            self.reference_pts_label.setText('(E, s): (MeV, mm)')
+
+    def clear_graph(self):
+        self.dnde_image.clear()
+
 
     def enable_disable_controls(self):
         '''
@@ -202,29 +294,36 @@ class Spectrometer_Interface(QMainWindow):
             self.locked_unlocked.setText('Unlocked')
         else:
             self.locked_unlocked.setText('Locked')
-        self.min_cutoff_energy_control.setEnabled(self.enable_controls.isChecked())
-        self.max_cutoff_energy_control.setEnabled(self.enable_controls.isChecked())
+        self.min_cutoff_energy_ctl.setEnabled(self.enable_controls.isChecked())
+        self.max_cutoff_energy_ctl.setEnabled(self.enable_controls.isChecked())
         self.flip_image.setEnabled(self.enable_controls.isChecked())
-        self.lanex_offset_mm_control.setEnabled(self.enable_controls.isChecked())
+        self.lanex_offset_mm_ctl.setEnabled(self.enable_controls.isChecked())
         self.config_path_button.setEnabled(self.enable_controls.isChecked())
         self.config_path_box.setEnabled(self.enable_controls.isChecked())
+        self.px_per_mm_ctl.setEnabled(self.enable_controls.isChecked())
+        self.mrad_per_px_ctl.setEnabled(self.enable_controls.isChecked())
+        self.pC_per_count_ctl.setEnabled(self.enable_controls.isChecked())
+        self.reference_method.setEnabled(self.enable_controls.isChecked())
+        self.energy_resolution_ctl.setEnabled(self.enable_controls.isChecked())
+        self.refpoint_x_or_energy.setEnabled(self.enable_controls.isChecked())
+        self.refpoint_y_or_s.setEnabled(self.enable_controls.isChecked())
 
     def change_energy_bounds(self) -> None:
         '''
         Change energy bouds for spectrum statistics and integration
         :return: None
         '''
-        self.min_cutoff_energy = self.min_cutoff_energy_control.value()
-        self.max_cutoff_energy = self.max_cutoff_energy_control.value()
-        print(f'Changed bounds')
+        self.min_cutoff_energy = self.min_cutoff_energy_ctl.value()
+        self.max_cutoff_energy = self.max_cutoff_energy_ctl.value()
+
 
     def change_lanex_offset_mm(self) -> None:
         '''
         Change offset with respect to zero or reference point (manual offset or motorized)
         :return: None
         '''
-        self.lanex_offset_mm = self.lanex_offset_mm_control.value()
-        self.dnde_image.clear()
+        self.lanex_offset_mm = self.lanex_offset_mm_ctl.value()
+        self.clear_graph()
         self.load_calib()
         self.graph_setup()
 
@@ -232,7 +331,6 @@ if __name__ == "__main__":
 
     appli = QApplication(sys.argv)
     appli.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyqt6'))
-    file= str(pathlib.Path(__file__).parents[0])+'/tir_025.TIFF'
-    e =Spectrometer_Interface()
+    e=Spectrometer_Interface()
     e.show()
     appli.exec_()
--- visu/spectrum_analysis/winSpectro.py
+++ visu/spectrum_analysis/winSpectro.py
@@ -3,17 +3,11 @@
 """
 Created on 2025/12/23
 @author: Aline Vernier
-Spectrum deconvolution + make data available
+Spectrum deconvolution + make data available for diagServ
 """
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget, QHBoxLayout, QGridLayout
-from PyQt6.QtWidgets import (QLabel, QMainWindow, QFileDialog, QStatusBar,
-                             QCheckBox, QDoubleSpinBox, QSlider, QPushButton, QLineEdit)
-from PyQt6 import QtCore, QtGui
-from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QAction
-from PyQt6.QtGui import QIcon, QShortcut, QFont
+from PyQt6.QtWidgets import QApplication
+from PyQt6 import QtCore
 import sys
-import pyqtgraph as pg
 import numpy as np
 import qdarkstyle
 import os
@@ -23,7 +17,7 @@ from visu.spectrum_analysis import Deconvolve_Spectrum as Deconvolve
 from visu.spectrum_analysis import Spectrum_Features
 from visu.spectrum_analysis import Build_Interface
 
-sys.path.insert(1, 'spectrum_analysis')
+sys.path.insert(1, '')
 sepa = os.sep
 
 class WINSPECTRO(Build_Interface.Spectrometer_Interface):
@@ -46,9 +40,7 @@ class WINSPECTRO(Build_Interface.Spectrometer_Interface):
         self.icon = str(p.parent) + sepa + 'icons' + sepa
         self.data_dict = {}
 
-        # Main window setup
-        #self.setup()
-        self.action_button()
+        # (Main window setup from parent class)
 
         # Load calibration data
         self.load_calib()
@@ -63,7 +55,7 @@ class WINSPECTRO(Build_Interface.Spectrometer_Interface):
     def load_calib(self):
         # Load calibration for spectrum deconvolution
         p = pathlib.Path(__file__)
-        self.deconv_calib = str(p.parent) + sepa + 'spectrum_analysis' + sepa
+        self.deconv_calib = str(p.parent) + sepa
         self.calibration_data = Deconvolve.CalibrationData(cal_path=self.deconv_calib + 'dsdE_default.txt')
         self.calibration_data_json = None
         # Create initialization object for spectrum deconvolution
@@ -71,10 +63,11 @@ class WINSPECTRO(Build_Interface.Spectrometer_Interface):
                                                       'magnet0.4T_Soectrum_isat4.9cm_26bar_gdd25850_HeAr_0002.TIFF',
                                               revert=True)
         self.deconvolved_spectrum = Deconvolve.DeconvolvedSpectrum(initImage, self.calibration_data,
-                                                                   0.5, 20.408, 0.1,
+                                                                   self.energy_resolution_ctl.value(),
+                                                                   20.408, 0.1,
                                                                    "zero", (1953, 635),
                                                                    4.33e-6,
-                                                                   offset=self.lanex_offset_mm_control.value())
+                                                                   offset=self.lanex_offset_mm_ctl.value())
 
     def graph_setup(self):
 
@@ -125,7 +118,8 @@ class WINSPECTRO(Build_Interface.Spectrometer_Interface):
         self.spectro_data_dict = Spectrum_Features.build_dict(self.deconvolved_spectrum.energy,
                                                               self.deconvolved_spectrum.integrated_spectrum,
                                                               temp_dataArray[1],
-                                                              energy_bounds=[self.min_cutoff_energy, self.max_cutoff_energy])
+                                                              energy_bounds=[self.min_cutoff_energy,
+                                                                             self.max_cutoff_energy])
         self.signalSpectroDict.emit(self.spectro_data_dict) # Signal for DiagServ
 
 
